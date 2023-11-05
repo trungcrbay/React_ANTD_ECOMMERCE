48a7bdb09d96560e94c789e5401a41834a60b8cd
Nguyen Dang Trung <trungnguyenjr123@gmail.com>
2023-11-05 14:11:49 +0700

    upload

--- src/components/Admin/LayoutAdmin.jsx
+++ src/components/Admin/LayoutAdmin.jsx
@@ -151,7 +151,7 @@ const LayoutAdmin = () => {
           <Outlet className="layout_action--icon" />
         </Content>
         <Footer style={{ padding: 0 }}>
-          React Test Fresher &copy; Hỏi Dân IT - Made with <HeartTwoTone />
+          React  &copy; TRUNG CR7 - Made with <HeartTwoTone />
         </Footer>
       </Layout>
     </Layout>
